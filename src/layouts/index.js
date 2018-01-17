63df70801e49c63b99800903de0e128af566d076
Phil Czek <phil.czek@gmail.com>
2018-01-17 13:51:48 +0100

    leaves only necessary css statements in the css file

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,18 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-// import './index.css'
+import './atem.css'
+
+const styles = {
+  container: {
+    backgroundColor: '#4B4B4B',
+    color: '#D9D9D9',
+    imageRendering: 'pixelated',
+    fontSize: '20px',
+    fontFamily: 'FantasqueSansMonoRegular, serif',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+  }
+}
 
 const TemplateWrapper = ({children}) => (
   <div>
-    <Helmet
-      title="atem.io / Philip Poloczek"
-      meta={[
-        {name: 'description', content: 'Sample'},
-        {name: 'keywords', content: 'sample, something'},
-      ]}
-    />
-    <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>atem.io / Philip Poloczek</title>
+      <meta name='description' content='Sample' />
+      <meta name='keywords' content='sample, something' />
+      <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/fantasque-sans-mono" type="text/css" />
+    </Helmet>
+    <div style={styles.container}>
       {children()}
     </div>
   </div>
