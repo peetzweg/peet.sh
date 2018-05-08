370de17b0de35db11b37b6762626e5280e4f7512
Phil Czek <phil.czek@gmail.com>
2018-05-08 22:14:27 +0200

    adds globe as a project

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -1,58 +1,63 @@
 export const ProjectData = [
-  {
-    title: 'Monota',
-    url:'https://github.com/Pczek/Monota',
-    createdAt: new Date(2017, 11, 1)
-  },
-  {
-    title: 'Aves',
-    url: 'http://getaves.com/',
-    createdAt: new Date(2017, 5, 1)
-  },
-  {
-    title: 'papr',
-    url: 'https://github.com/Pczek/papr',
-    createdAt: new Date(2017, 1, 1)
-  },
-  {
-    title: 'diarydash',
-    url: 'http://diarydash.com/',
-    createdAt: new Date(2017, 0, 1)
-  },
-  {
-    title: 'breathpacer',
-    url: 'https://pczek.github.io/breathpacer/',
-    createdAt: new Date(2016, 6, 1)
-  },
-  {
-    title: 'matgen',
-    url: 'https://pczek.github.io/matgen/',
-    createdAt: new Date(2016, 2, 1)
-  },
-  {
-    title: 'Five_Minute_Journal',
-    url: 'https://pczek.github.io/5mJ/',
-    createdAt: new Date(2015, 6, 1)
-  },
-  {
-    title: 'Commander_Ottens_Pro_Longboarding',
-    url: 'https://github.com/Pczek/COPL',
-    createdAt: new Date(2015, 1, 1)
-  },
-  {
-    title: 'Nuclear_Power_Plant_Simulator_2014',
-    url: 'https://github.com/Pczek/NPPS2014',
-    createdAt: new Date(2015, 11, 1)
-  },
-  {
-    title: 'cairobou',
-    url: 'https://github.com/Pczek/cairobou',
-    createdAt: new Date(2014, 9, 1)
-  },
-]
+	{
+		title: 'Globe - Connected Ether Tracker',
+		url: 'https://github.com/Pczek/Globe',
+		createdAt: new Date(2018, 2, 15)
+	},
+	{
+		title: 'Monota - Mono Tasking App',
+		url: 'https://github.com/Pczek/Monota',
+		createdAt: new Date(2017, 11, 1)
+	},
+	{
+		title: 'Aves -  Setupless Text2Speech for Blogs',
+		url: 'http://getaves.com/',
+		createdAt: new Date(2017, 5, 1)
+	},
+	{
+		title: 'papr - Paper Calendar Templates',
+		url: 'https://github.com/Pczek/papr',
+		createdAt: new Date(2017, 1, 1)
+	},
+	{
+		title: 'diarydash - Daily Diary Dashboard',
+		url: 'http://diarydash.com/',
+		createdAt: new Date(2017, 0, 1)
+	},
+	{
+		title: 'breathpacer - Rythmic Breath Pacer',
+		url: 'https://pczek.github.io/breathpacer/',
+		createdAt: new Date(2016, 6, 1)
+	},
+	{
+		title: 'matgen - Cutting Mat Background Generator',
+		url: 'https://pczek.github.io/matgen/',
+		createdAt: new Date(2016, 2, 1)
+	},
+	{
+		title: 'Five_Minute_Journal',
+		url: 'https://pczek.github.io/5mJ/',
+		createdAt: new Date(2015, 6, 1)
+	},
+	{
+		title: 'Commander_Ottens_Pro_Longboarding',
+		url: 'https://github.com/Pczek/COPL',
+		createdAt: new Date(2015, 1, 1)
+	},
+	{
+		title: 'Nuclear_Power_Plant_Simulator_2014',
+		url: 'https://github.com/Pczek/NPPS2014',
+		createdAt: new Date(2015, 11, 1)
+	},
+	{
+		title: 'cairobou - Creative Coding with Cairo',
+		url: 'https://github.com/Pczek/cairobou',
+		createdAt: new Date(2014, 9, 1)
+	}
+];
 export const ProfileData = [
-  {title: 'LinkedIn', url: 'https://www.linkedin.com/in/philip-poloczek-1b3202125/'},
-  {title: 'GitHub', url: 'https://github.com/Pczek'},
-  {title: 'STRAVA', url: 'https://www.strava.com/athletes/18353420'},
-  {title: 'Instagram', url: 'https://www.instagram.com/naolimao/'},
-]
+	{ title: 'LinkedIn', url: 'https://www.linkedin.com/in/philip-poloczek-1b3202125/' },
+	{ title: 'GitHub', url: 'https://github.com/Pczek' },
+	{ title: 'STRAVA', url: 'https://www.strava.com/athletes/18353420' },
+	{ title: 'Instagram', url: 'https://www.instagram.com/naolimao/' }
+];
