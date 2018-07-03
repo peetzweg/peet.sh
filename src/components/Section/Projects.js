304062583022f5076a407d1715d95b8dca4b592a
Phil Czek <phil.czek@gmail.com>
2018-07-03 11:38:41 +0200

    shortens links for mobile. Not yet able to make proper use of a media query here.

--- src/components/Section/Projects.js
+++ src/components/Section/Projects.js
@@ -4,26 +4,23 @@ import SoftLink from '../Terminal/SoftLink';
 import List from '../Terminal/List';
 import { ProjectData } from '../Data';
 
-const Projects = () => {
-	const formLinkTitle = (project) =>
-		window.innerWidth > 500
-			? project.description
-				? `${project.title.toUpperCase()} - ${project.description}`
-				: `${project.title.toUpperCase()}`
-			: `${project.title.toUpperCase()}`;
-
-	return (
-		<List long name={'Projects'}>
-			{ProjectData.map((project) => (
-				<SoftLink
-					verbose={window.innerWidth > 500}
-					key={`${project.title}_Project`}
-					title={formLinkTitle(project)}
-					url={project.url}
-				/>
-			))}
-		</List>
-	);
-};
+const Projects = () => (
+	<List long name={'Projects'}>
+		{ProjectData.map((project) => (
+			<SoftLink
+				verbose={false}
+				key={`${project.title}_Project`}
+				title={
+					project.description ? (
+						`${project.title.toUpperCase()} - ${project.description}`
+					) : (
+						`${project.title.toUpperCase()}`
+					)
+				}
+				url={project.url}
+			/>
+		))}
+	</List>
+);
 
 export default Projects;
