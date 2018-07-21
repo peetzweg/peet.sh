6a280ed8628119b673c255179a41e6f8cc265e7d
Phil Czek <phil.czek@gmail.com>
2018-07-21 14:45:56 +0200

    adds my hackerrank profile

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -81,6 +81,7 @@ export const ProjectData = [
 export const ProfileData = [
 	{ title: 'LinkedIn', url: 'https://www.linkedin.com/in/philip-poloczek-1b3202125/' },
 	{ title: 'GitHub', url: 'https://github.com/Pczek' },
+	{ title: 'Hackerrank', url: 'https://www.hackerrank.com/pczek' },
 	{ title: 'STRAVA', url: 'https://www.strava.com/athletes/18353420' },
 	{ title: 'Instagram', url: 'https://www.instagram.com/naolimao/' }
 ];
