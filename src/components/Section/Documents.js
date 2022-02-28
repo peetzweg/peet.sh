ac911c4cd8ae97c80f69fb3728ca68381b11d113
pczek <pczek@users.noreply.github.com>
2022-02-28 10:31:00 +0100

    links out to client work on notion

--- src/components/Section/Documents.js
+++ src/components/Section/Documents.js
@@ -1,15 +1,21 @@
-import React from 'react'
-
-import List from '../Terminal/List'
-import File from '../Terminal/File'
-import Resume from '../../documents/resume_poloczek_philip.pdf'
+import React from "react";
+import Resume from "../../documents/resume_poloczek_philip.pdf";
+import File from "../Terminal/File";
+import List from "../Terminal/List";
+import SoftLink from "../Terminal/SoftLink";
 
 const Documents = () => {
   return (
-    <List name={'Documents'}>
-      <File name={'resume_poloczek.pdf'} url={Resume} />
+    <List name={"Documents"}>
+      <File name={"resume_poloczek.pdf"} url={Resume} />
+      <SoftLink
+        title={"client_work"}
+        url={
+          "https://atemio.notion.site/Client-Work-504f7a0b831841f7b71c50bbe8b6f368"
+        }
+      />
     </List>
-  )
-}
+  );
+};
 
-export default Documents
+export default Documents;
