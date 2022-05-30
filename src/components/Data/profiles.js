64fcbbae5b5367d349f1368ddefd3a1c4e9be718
pczek <pczek@users.noreply.github.com>
2022-05-30 21:30:00 +0200

    change pczek to peetzweg

--- src/components/Data/profiles.js
+++ src/components/Data/profiles.js
@@ -7,8 +7,8 @@ export default [
     title: "LinkedIn",
     url: "https://www.linkedin.com/in/philip-poloczek-1b3202125/",
   },
-  { title: "GitHub", url: "https://github.com/Pczek" },
-  // { title: "Hackerrank", url: "https://www.hackerrank.com/pczek" },
+  { title: "GitHub", url: "https://github.com/peetzweg" },
+  // { title: "Hackerrank", url: "https://www.hackerrank.com/peetzweg" },
   { title: "STRAVA", url: "https://www.strava.com/athletes/18353420" },
   // { title: "Instagram", url: "https://www.instagram.com/naolimao/" }
 ];
--- src/components/Data/projects.js
+++ src/components/Data/projects.js
@@ -1,98 +1,98 @@
 export default [
-    {
-      title: "Offwhite",
-      description: "Store for Stationary Templates",
-      url: "https://www.instagram.com/offwhiteapp",
-      createdAt: new Date(2020, 1, 28)
-    },
-    {
-      title: "loop",
-      description: "Mac App to Keep Track of Your Social Interactions",
-      url: "/loop",
-      createdAt: new Date(2019, 6, 10)
-    },
-    {
-      title: "Polos Pretty Paper",
-      description: "Print Templates",
-      url: "https://itunes.apple.com/us/app/polos-pretty-paper/id1439946484?mt=8",
-      createdAt: new Date(2018, 12, 1)
-    },
-    {
-      title: "Airbox",
-      description: "Audio Inbox for your Smart Speaker",
-      url: "http://airbox.network",
-      createdAt: new Date(2018, 4, 1)
-    },
-    {
-      title: "Parcel Explorer App",
-      description: "Google Maps for Decentraland",
-      url: "https://github.com/Pczek/ParcelExplorer",
-      createdAt: new Date(2018, 3, 1)
-    },
-    {
-      title: "Globe",
-      description: "Connected Ether Tracker/Wallet",
-      url: "https://github.com/Pczek/Globe",
-      createdAt: new Date(2018, 2, 15)
-    },
-    {
-      title: "Monota",
-      description: "Mono Tasking iOS App",
-      url: "https://itunes.apple.com/us/app/monota/id1333036184?mt=8",
-      createdAt: new Date(2017, 11, 1)
-    },
-    {
-      title: "Aves",
-      description: "Setupless Text2Speech for Blogs",
-      url: "http://getaves.com.s3-website-eu-west-1.amazonaws.com/introduction/",
-      createdAt: new Date(2017, 5, 1)
-    },
-    {
-      title: "papr",
-      description: "Paper Calendar Templates",
-      url: "https://github.com/Pczek/papr",
-      createdAt: new Date(2017, 1, 1)
-    },
-    {
-      title: "diarydash",
-      description: "Daily Diary Dashboard",
-      url: "http://diarydash.com/",
-      createdAt: new Date(2017, 0, 1)
-    },
-    {
-      title: "breathpacer",
-      description: "Rythmic Breath Pacer",
-      url: "https://pczek.github.io/breathpacer/",
-      createdAt: new Date(2016, 6, 1)
-    },
-    {
-      title: "matgen",
-      description: "Cutting Mat Background Generator",
-      url: "https://pczek.github.io/matgen/",
-      createdAt: new Date(2016, 2, 1)
-    },
-    {
-      title: "Five_Minute_Journal",
-      description: "",
-      url: "https://pczek.github.io/5mJ/",
-      createdAt: new Date(2015, 6, 1)
-    },
-    {
-      title: "Commander_Ottens_Pro_Longboarding",
-      description: "",
-      url: "https://github.com/Pczek/COPL",
-      createdAt: new Date(2015, 1, 1)
-    },
-    {
-      title: "Nuclear_Power_Plant_Simulator_2014",
-      description: "",
-      url: "https://github.com/Pczek/NPPS2014",
-      createdAt: new Date(2015, 11, 1)
-    },
-    {
-      title: "cairobou",
-      description: "Creative Coding with Cairo",
-      url: "https://github.com/Pczek/cairobou",
-      createdAt: new Date(2014, 9, 1)
-    }
-  ];
+  {
+    title: "Offwhite",
+    description: "Store for Stationary Templates",
+    url: "https://www.instagram.com/offwhiteapp",
+    createdAt: new Date(2020, 1, 28),
+  },
+  {
+    title: "loop",
+    description: "Mac App to Keep Track of Your Social Interactions",
+    url: "/loop",
+    createdAt: new Date(2019, 6, 10),
+  },
+  {
+    title: "Polos Pretty Paper",
+    description: "Print Templates",
+    url: "https://itunes.apple.com/us/app/polos-pretty-paper/id1439946484?mt=8",
+    createdAt: new Date(2018, 12, 1),
+  },
+  {
+    title: "Airbox",
+    description: "Audio Inbox for your Smart Speaker",
+    url: "http://airbox.network",
+    createdAt: new Date(2018, 4, 1),
+  },
+  {
+    title: "Parcel Explorer App",
+    description: "Google Maps for Decentraland",
+    url: "https://github.com/peetzweg/ParcelExplorer",
+    createdAt: new Date(2018, 3, 1),
+  },
+  {
+    title: "Globe",
+    description: "Connected Ether Tracker/Wallet",
+    url: "https://github.com/peetzweg/Globe",
+    createdAt: new Date(2018, 2, 15),
+  },
+  {
+    title: "Monota",
+    description: "Mono Tasking iOS App",
+    url: "https://itunes.apple.com/us/app/monota/id1333036184?mt=8",
+    createdAt: new Date(2017, 11, 1),
+  },
+  {
+    title: "Aves",
+    description: "Setupless Text2Speech for Blogs",
+    url: "http://getaves.com.s3-website-eu-west-1.amazonaws.com/introduction/",
+    createdAt: new Date(2017, 5, 1),
+  },
+  {
+    title: "papr",
+    description: "Paper Calendar Templates",
+    url: "https://github.com/peetzweg/papr",
+    createdAt: new Date(2017, 1, 1),
+  },
+  {
+    title: "diarydash",
+    description: "Daily Diary Dashboard",
+    url: "http://diarydash.com/",
+    createdAt: new Date(2017, 0, 1),
+  },
+  {
+    title: "breathpacer",
+    description: "Rythmic Breath Pacer",
+    url: "https://peetzweg.github.io/breathpacer/",
+    createdAt: new Date(2016, 6, 1),
+  },
+  {
+    title: "matgen",
+    description: "Cutting Mat Background Generator",
+    url: "https://peetzweg.github.io/matgen/",
+    createdAt: new Date(2016, 2, 1),
+  },
+  {
+    title: "Five_Minute_Journal",
+    description: "",
+    url: "https://peetzweg.github.io/5mJ/",
+    createdAt: new Date(2015, 6, 1),
+  },
+  {
+    title: "Commander_Ottens_Pro_Longboarding",
+    description: "",
+    url: "https://github.com/peetzweg/COPL",
+    createdAt: new Date(2015, 1, 1),
+  },
+  {
+    title: "Nuclear_Power_Plant_Simulator_2014",
+    description: "",
+    url: "https://github.com/peetzweg/NPPS2014",
+    createdAt: new Date(2015, 11, 1),
+  },
+  {
+    title: "cairobou",
+    description: "Creative Coding with Cairo",
+    url: "https://github.com/peetzweg/cairobou",
+    createdAt: new Date(2014, 9, 1),
+  },
+];
