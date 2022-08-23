b8753a72a59b9265733365c92f1960349369b3ca
pczek <839848+peetzweg@users.noreply.github.com>
2022-08-23 15:50:21 +0200

    adds goodreads

--- src/components/Data/profiles.js
+++ src/components/Data/profiles.js
@@ -8,7 +8,9 @@ export default [
     url: "https://www.linkedin.com/in/philip-poloczek-1b3202125/",
   },
   { title: "GitHub", url: "https://github.com/peetzweg" },
-  // { title: "Hackerrank", url: "https://www.hackerrank.com/peetzweg" },
   { title: "STRAVA", url: "https://www.strava.com/athletes/18353420" },
-  // { title: "Instagram", url: "https://www.instagram.com/naolimao/" }
+  {
+    title: "goodreads",
+    url: "https://www.goodreads.com/user/show/150027885-peetzweg",
+  },
 ];
