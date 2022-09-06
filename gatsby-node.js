eb6beb4777030e1fb9b84f1cc1fcd2bd555fe1ba
pczek <839848+peetzweg@users.noreply.github.com>
2022-09-06 21:00:02 +0200

    redirect directly to readme section

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,18 @@
-exports.createPages = ({  actions }) => {
-    const {createRedirect} = actions
-    createRedirect({ fromPath: '/web3-vscode-extension', toPath: 'https://github.com/peetzweg/vscode-crypto-address-lens', isPermanent: true });
-    createRedirect({ fromPath: '/web3-cli', toPath: 'https://github.com/peetzweg/notar', isPermanent: true });
-    createRedirect({ fromPath: '/ABImate', toPath: 'https://github.com/peetzweg/abimate', isPermanent: true });
-  }
+exports.createPages = ({ actions }) => {
+  const { createRedirect } = actions;
+  createRedirect({
+    fromPath: "/web3-vscode-extension",
+    toPath: "https://github.com/peetzweg/vscode-crypto-address-lens#readme",
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: "/web3-cli",
+    toPath: "https://github.com/peetzweg/notar#readme",
+    isPermanent: true,
+  });
+  createRedirect({
+    fromPath: "/ABImate",
+    toPath: "https://github.com/peetzweg/abimate#readme",
+    isPermanent: true,
+  });
+};
