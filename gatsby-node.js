badd60c1bb5d32e39247f2cbbe2006f5be776215
pczek <839848+peetzweg@users.noreply.github.com>
2022-11-23 10:04:49 +0100

    make redirect for client-work

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,4 +15,9 @@ exports.createPages = ({ actions }) => {
     toPath: "https://github.com/peetzweg/abimate#readme",
     isPermanent: true,
   });
+  createRedirect({
+    fromPath: "/client-work",
+    toPath: "https://peetzweg.notion.site/peetzweg/Client-Work-504f7a0b831841f7b71c50bbe8b6f368",
+    isPermanent: true,
+  });
 };
