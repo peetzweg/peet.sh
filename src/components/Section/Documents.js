9a0e48b94fea57b6f060199f36d4cec1f0646471
pczek <839848+peetzweg@users.noreply.github.com>
2023-01-05 20:08:36 +0100

    removes easy accessible CV from website

--- src/components/Section/Documents.js
+++ src/components/Section/Documents.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Resume from "../../documents/resume_poloczek_philip.pdf";
-import File from "../Terminal/File";
 import List from "../Terminal/List";
 import SoftLink from "../Terminal/SoftLink";
 
 const Documents = () => {
   return (
     <List name={"Documents"}>
-      <File name={"resume_poloczek.pdf"} url={Resume} />
       <SoftLink
         title={"client_work"}
         url={
