3dca802c193570476fc01dcc2eebf96afc6e1ef7
pczek <839848+peetzweg@users.noreply.github.com>
2023-01-06 12:16:52 +0100

    adds terms of service and privacy policy for `input`

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,12 +17,22 @@ exports.createPages = ({ actions }) => {
   });
   createRedirect({
     fromPath: "/client-work",
-    toPath: "https://peetzweg.notion.site/peetzweg/Client-Work-504f7a0b831841f7b71c50bbe8b6f368",
+    toPath: "https://peetzweg.notion.site/peetzweg/504f7a0b831841f7b71c50bbe8b6f368",
     isPermanent: true,
   });
   createRedirect({
     fromPath: "/software",
-    toPath: "https://peetzweg.notion.site/Software-Side-Projects-e006cc11b7fb4c0d880d986a30e65e0e",
+    toPath: "https://peetzweg.notion.site/e006cc11b7fb4c0d880d986a30e65e0e",
+    isPermanent: false,
+  });
+  createRedirect({
+    fromPath: "/product/input/terms-of-service",
+    toPath: "https://peetzweg.notion.site/8b490e6ef2324ba1ad8d122ddd971838",
+    isPermanent: false,
+  });
+  createRedirect({
+    fromPath: "/product/input/privacy-policy",
+    toPath: "https://peetzweg.notion.site/54d02ca9dcc84d39bcfbd1eed867b904",
     isPermanent: false,
   });
 };
