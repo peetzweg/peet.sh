4e8752dcc9428b9e0c8f2735173cf9e762b87edb
pczek <839848+peetzweg@users.noreply.github.com>
2023-01-16 18:59:47 +0100

    reorders profiles and adds reddit

--- src/components/Data/profiles.js
+++ src/components/Data/profiles.js
@@ -1,17 +1,21 @@
 const profiles = [
   {
-    title: "Twitter",
-    url: "https://twitter.com/peetzweg",
+    title: "reddit",
+    url: "https://www.reddit.com/user/pfilzweg",
   },
   {
-    title: "LinkedIn",
-    url: "https://www.linkedin.com/in/philip-poloczek-1b3202125/",
+    title: "Twitter",
+    url: "https://twitter.com/peetzweg",
   },
   { title: "GitHub", url: "https://github.com/peetzweg" },
   { title: "STRAVA", url: "https://www.strava.com/athletes/18353420" },
   {
     title: "goodreads",
     url: "https://www.goodreads.com/peetzweg",
+  },
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/philip-poloczek-1b3202125/",
   },
 ];
 export default profiles;
