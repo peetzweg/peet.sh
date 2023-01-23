603c647f06a029658ea1079db63745be935d6c0c
pczek <839848+peetzweg@users.noreply.github.com>
2023-01-23 18:13:25 +0100

    adds new starting page with just the links

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -39,8 +39,96 @@ const IndexPage = () => {
         />
       </Helmet>
 
-      <PixelMeImage src={PixelMe} alt="" />
       <Page>
+        <div style={{ position: "fixed", top: 16, left: 16 }}>◠◠</div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignContent: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div></div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                }}
+              >
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <div>peet.sh /</div>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <br />
+                  <a href={"/software"}>
+                    <div>software</div>
+                  </a>
+                  <a href={"/client-work"}>
+                    <div>client-work</div>
+                  </a>
+                  <a href={"/hardware"}>
+                    <div>hardware</div>
+                  </a>
+                  <div>profiles /</div>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <br />
+
+                  <br />
+
+                  <br />
+
+                  <br />
+                  <a href={"https://github.com/peetzweg"}>
+                    <div>github</div>
+                  </a>
+                  <a href={"https://twitter.com/peetzweg"}>
+                    <div>twitter</div>
+                  </a>
+                  <a href={"https://www.linkedin.com/in/peetzweg/"}>
+                    <div>linkedin</div>
+                  </a>
+                  <a href={"https://www.strava.com/athletes/18353420"}>
+                    <div>strava</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginBottom: 16,
+              }}
+            >
+              <div>scroll for old website</div>
+              <div>↓</div>
+            </div>
+          </div>
+        </div>
+      </Page>
+      <Page>
+        <PixelMeImage src={PixelMe} alt="" />
         <Writings />
         <TerminalComponent />
       </Page>
