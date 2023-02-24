9615bcc776e9fa8df0f210f13dcf2d88fa384bb4
pczek <839848+peetzweg@users.noreply.github.com>
2023-02-24 15:54:18 +0100

    cleanup to add tulip back in

--- libs/feat/portfolio-models/src/lib/feat-portfolio-models.tsx
+++ libs/feat/portfolio-models/src/lib/feat-portfolio-models.tsx
@@ -7,39 +7,15 @@ import { Group, BoxGeometry, MeshStandardMaterial } from 'three';
 /* eslint-disable-next-line */
 export interface FeatPortfolioModelsProps {}
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += delta));
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  );
-}
-
 const RotatingTulip = () => {
   const mesh = useRef<Group>();
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     if (mesh.current) {
       mesh.current.rotation.y += delta;
       mesh.current.rotation.x += delta / 10;
     }
   });
-  return <Tulip ref={mesh as any} scale={4} />;
+  return <Tulip ref={mesh as any} />;
 };
 
 export function FeatPortfolioModels(props: FeatPortfolioModelsProps) {
