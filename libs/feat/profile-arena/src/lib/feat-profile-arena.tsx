f4432d798e14fb86d7bcdbd1e11bc1e6738590a5
pczek <839848+peetzweg@users.noreply.github.com>
2023-02-24 15:50:41 +0100

    floating orb in arena profile testing.

--- libs/feat/profile-arena/src/lib/feat-profile-arena.tsx
+++ libs/feat/profile-arena/src/lib/feat-profile-arena.tsx
@@ -1,4 +1,38 @@
+import { Environment, Float, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
 export interface FeatProfileArenaProps {}
+
+const Scene = () => {
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <OrbitControls
+        makeDefault
+        autoRotate
+        enableDamping
+        rotateSpeed={1}
+        autoRotateSpeed={4}
+        enableZoom={false}
+      />
+
+      <Float floatIntensity={1} speed={1} floatingRange={[-0.05, 0.05]}>
+        <mesh
+          scale={0.7}
+          castShadow
+          position={[0, 0, 0]}
+          rotation={[0, 0, 0.5]}
+        >
+          <meshStandardMaterial color="#C8A86E" metalness={0} roughness={1} />
+
+          <sphereGeometry args={[1, 128, 128]} />
+        </mesh>
+      </Float>
+
+      <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr" />
+    </>
+  );
+};
 
 export function FeatProfileArena(props: FeatProfileArenaProps) {
   return (
@@ -13,6 +47,7 @@ export function FeatProfileArena(props: FeatProfileArenaProps) {
       }}
     >
       <div style={{ position: 'fixed', top: 16, left: 16 }}>◠◠</div>
+
       <div
         style={{
           width: '100%',
@@ -20,6 +55,8 @@ export function FeatProfileArena(props: FeatProfileArenaProps) {
           flexDirection: 'column',
           alignContent: 'center',
           justifyContent: 'center',
+          fontWeight: '600',
+          lineHeight: 1.7,
         }}
       >
         <div
@@ -31,6 +68,19 @@ export function FeatProfileArena(props: FeatProfileArenaProps) {
             justifyContent: 'space-between',
           }}
         >
+          <Canvas
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              zIndex: -1,
+              height: '100vh',
+              width: '100vw',
+            }}
+            camera={{ fov: 90, near: 0.1, far: 10, position: [0.5, 0.5, 1] }}
+          >
+            <Scene />
+          </Canvas>
           <div></div>
           <div
             style={{
