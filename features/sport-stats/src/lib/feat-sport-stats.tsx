d663cab393d02edc2395934266993b1cca4211f7
pczek <839848+peetzweg@users.noreply.github.com>
2023-11-19 18:13:16 +0100

    use tailwind in sports feature

--- features/sport-stats/src/lib/feat-sport-stats.tsx
+++ features/sport-stats/src/lib/feat-sport-stats.tsx
@@ -1,37 +1,17 @@
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import { AccumulatedGraph } from './AccumulatedGraph';
-import styles from './feat-sport-stats.module.css';
 
 /* eslint-disable-next-line */
 export interface FeatSportStatsProps {}
 
 export function FeatSportStats(props: FeatSportStatsProps) {
   return (
-    <div
-      className={styles['container']}
-      style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <div
-        style={{
-          width: '90vw',
-          height: '90vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <ParentSize>
-          {({ width, height }) => (
-            <AccumulatedGraph width={width} height={height} />
-          )}
-        </ParentSize>
-      </div>
+    <div className="v-screen h-screen flex justify-center items-center p-8">
+      <ParentSize>
+        {({ width, height }) => (
+          <AccumulatedGraph width={width} height={height} />
+        )}
+      </ParentSize>
     </div>
   );
 }
