ffb0b8475c262263cfa9326d4e17a8eab8df0ae6
pczek <839848+peetzweg@users.noreply.github.com>
2024-01-14 16:37:01 +0100

    adds template feature which can easily be copied to start a new feature

---
+++ features/template/src/lib/feat-template.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface FeatTemplateProps {}
+
+export function FeatTemplate(props: FeatTemplateProps) {
+  return (
+    <div className="v-screen h-screen flex justify-center items-center p-8">
+      <h1>Template Feature</h1>
+    </div>
+  );
+}
+
+export default FeatTemplate;
