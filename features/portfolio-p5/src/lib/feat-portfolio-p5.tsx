aa19fe5e36bd7a3b6d08028465c19f74c1e085c0
peetzweg/ <839848+peetzweg@users.noreply.github.com>
2024-02-22 21:48:55 +0100

    animates chromiesquiggle (#40)

--- features/portfolio-p5/src/lib/feat-portfolio-p5.tsx
+++ features/portfolio-p5/src/lib/feat-portfolio-p5.tsx
@@ -4,13 +4,23 @@ import Subscapes from './sketches/Subscapes';
 /* eslint-disable-next-line */
 export interface FeatPortfolioP5Props {}
 
-export function FeatPortfolioP5(props: FeatPortfolioP5Props) {
+export function Feature(props: FeatPortfolioP5Props) {
   return (
-    <div className="h-screen flex flex-row flex-wrap justify-around items-center overflow-hidden">
+    <div className="h-auto flex flex-col flex-wrap justify-around items-center overflow-hidden">
+      <div className="flex flex-col w-full md:w-1/2">
+        <ChromieSquiggle />
+        <a
+          href="https://github.com/peetzweg/peet.sh/blob/main/features/portfolio-p5/src/lib/sketches/ChromieSquiggle.tsx"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="underline font-mono"
+        >
+          ChromieSquiggle - source ↗
+        </a>
+      </div>
       <Subscapes />
-      <ChromieSquiggle />
     </div>
   );
 }
 
-export default FeatPortfolioP5;
+export default Feature;
