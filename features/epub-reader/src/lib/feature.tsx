4ba5c9a92c1ed6cf355594bf3f9c71bced982b38
peetzweg/ <phil.czek@gmail.com>
2024-08-14 11:53:58 +0200

    exploring file system api

--- features/epub-reader/src/lib/feature.tsx
+++ features/epub-reader/src/lib/feature.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Navigation } from './Navigation';
 import { Reader } from './Reader';
 import { useStore } from './store';
@@ -8,9 +8,45 @@ export function EpubReader() {
 
   return (
     <div className="flex flex-col pt-10 items-center w-screen min-h-screen ">
+      <BookPicker />
       <Navigation />
 
       {content && <Reader text={content} />}
     </div>
   );
 }
+
+const BookPicker = () => {
+  const [opfs, setOpfs] = useState<FileSystemDirectoryHandle | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    navigator.storage.getDirectory().then((dir) => {
+      console.log({ dir });
+      dir.getFileHandle('');
+      setOpfs(dir);
+    });
+  }, []);
+
+  const selectFile = useCallback(() => {
+    if (window.showOpenFilePicker) {
+      window.showOpenFilePicker().then((handle: FileSystemFileHandle[]) => {
+        console.log({ handle });
+        handle[0].getFile().then((file) => {
+          console.log({ file });
+        });
+        handle[0].requestPermission({ mode: 'read' }).then((perm) => {
+          console.log({ newperm: perm });
+        });
+        handle[0].queryPermission().then((perm) => {
+          console.log({ perm });
+        });
+      });
+    } else {
+      console.warn('window.showOpenFilePicker() not available');
+    }
+  }, []);
+
+  return <button onClick={selectFile}>choose file</button>;
+};
