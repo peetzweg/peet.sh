10e8bf24cfb351211b569287f3f4b2ada6b60551
peetzweg/ <phil.czek@gmail.com>
2024-08-14 12:55:45 +0200

    feat: epub reader now shows previous and future words on pause

--- features/epub-reader/src/lib/Reader.tsx
+++ features/epub-reader/src/lib/Reader.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useMemo, useState } from 'react';
 import { useStore } from './store';
+import { cn } from 'lib-utils';
 
 interface Props {
   text: string;
 }
+
 export const Reader = ({ text }: Props) => {
   const isPlaying = useStore((state) => state.isPlaying);
+  const pause = useStore((state) => state.pause);
   const setProgress = useStore((state) => state.setProgress);
   const [current, setCurrent] = useState<number>(0);
   const [words, total] = useMemo(() => {
@@ -24,7 +27,12 @@ export const Reader = ({ text }: Props) => {
       const newInterval = setInterval(() => {
         setCurrent((prev) => {
           const newCurrent = prev + 1;
+
           setProgress(newCurrent / total);
+          if (newCurrent >= total) {
+            clearInterval(newInterval);
+            pause();
+          }
           return newCurrent;
         });
       }, 250);
@@ -40,8 +48,28 @@ export const Reader = ({ text }: Props) => {
   // console.log(words[current]);
 
   return (
-    <div className="flex flex-col justify-center font-serif items-center w-full h-96 text-5xl ">
-      <h1>{`${words[current]}`}</h1>
+    <div className="flex flex-col justify-center font-serif items-center w-full">
+      <div className="w-full max-w-96 px-8 flex flex-col gap-1">
+        <div className="h-32 flex flex-col justify-end">
+          <p
+            className={cn('opacity-45 text-justify', {
+              hidden: isPlaying,
+            })}
+          >
+            {words.slice(Math.max(current - 20, 0), current).join(' ')}
+          </p>
+        </div>
+        <h1 className="text-5xl">{`${words[current]}`}</h1>
+        <div className="h-32 flex flex-col justify-start">
+          <p
+            className={cn('opacity-45 text-justify h-36', {
+              hidden: isPlaying || current === 0,
+            })}
+          >
+            {words.slice(Math.max(current + 1, 0), current + 20).join(' ')}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
